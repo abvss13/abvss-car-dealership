84b0875865b0c529f1b9c3f3b3b96b4e36787638
Your Name <abdullahi.abass@student.moringschool.com>
2023-08-11 20:30:54 +0300

    done

--- car-dealership/src/CarApp.js
+++ car-dealership/src/CarApp.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 
 function CarApp() {
   const [cars, setCars] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedBrand, setSelectedBrand] = useState('');
+  const [sortBy, setSortBy] = useState('price-asc');
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     const fetchCarData = async () => {
@@ -18,16 +23,71 @@ function CarApp() {
   }, []);
 
   const handleContactClick = (contactInfo) => {
-    alert(`Contact the dealer:\n${contactInfo}`);
+    setSelectedCar(contactInfo);
   };
+
+  const handleModalClose = () => {
+    setSelectedCar(null);
+  };
+
+  const handleAddToCart = (car) => {
+    setCart([...cart, car]);
+  };
+
+  const handleRemoveFromCart = (car) => {
+    const updatedCart = cart.filter((item) => item !== car);
+    setCart(updatedCart);
+  };
+
+  const filteredCars = cars.filter(
+    (car) =>
+      car.brand.toLowerCase().includes(selectedBrand.toLowerCase()) &&
+      (car.brand.toLowerCase().includes(searchQuery.toLowerCase()) ||
+        car.model.toLowerCase().includes(searchQuery.toLowerCase()))
+  );
+
+  const sortedCars = [...filteredCars].sort((a, b) => {
+    if (sortBy === 'price-asc') {
+      return parseFloat(a.price.replace(/[^0-9.-]+/g, '')) - parseFloat(b.price.replace(/[^0-9.-]+/g, ''));
+    } else if (sortBy === 'price-desc') {
+      return parseFloat(b.price.replace(/[^0-9.-]+/g, '')) - parseFloat(a.price.replace(/[^0-9.-]+/g, ''));
+    }
+    return 0;
+  });
 
   return (
     <div className="car-app">
       <header className="app-header">
         <h1>Car Dealership</h1>
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search by brand or model"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <select
+            value={selectedBrand}
+            onChange={(e) => setSelectedBrand(e.target.value)}
+          >
+            <option value="">All Brands</option>
+            {Array.from(new Set(cars.map((car) => car.brand))).map((brand, index) => (
+              <option key={index} value={brand}>
+                {brand}
+              </option>
+            ))}
+          </select>
+          <select
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+          >
+            <option value="price-asc">Price: Low to High</option>
+            <option value="price-desc">Price: High to Low</option>
+          </select>
+        </div>
       </header>
       <div className="car-list">
-        {cars.map((car, index) => (
+        {sortedCars.map((car, index) => (
           <div className="car-item" key={index}>
             <img src={car.img_url} alt={car.model} />
             <div className="car-details">
@@ -37,17 +97,36 @@ function CarApp() {
               <p>Acceleration: {car.acceleration}</p>
               <p>Maximum Speed: {car.maximum_speed}</p>
             </div>
-            <div className="contact-options">
+            <div className="action-buttons">
               <button
                 className="contact-button"
-                onClick={() => handleContactClick(`Telephone: +254798491946\nGmail: abdulabass1738@gmail.com`)}
+                onClick={() => handleContactClick(car)}
               >
                 Contact Dealer
               </button>
+              {cart.includes(car) ? (
+                <button className="cart-button remove" onClick={() => handleRemoveFromCart(car)}>
+                  Remove from Cart
+                </button>
+              ) : (
+                <button className="cart-button add" onClick={() => handleAddToCart(car)}>
+                  Add to Cart
+                </button>
+              )}
             </div>
           </div>
         ))}
       </div>
+      {selectedCar && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={handleModalClose}>&times;</span>
+            <h2>Contact Dealer</h2>
+            <p>Telephone: +254798491946</p>
+            <p>Gmail: abdulabass1738@gmail.com</p>
+          </div>
+        </div>
+      )}
       <footer className="app-footer">
         <p>&copy; 2023 Car Dealership. All rights reserved.</p>
       </footer>
